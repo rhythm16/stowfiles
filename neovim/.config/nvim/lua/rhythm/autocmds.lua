d3f976cf32c2a36f2b03df8a84ddfafb1650626f
Wei-Lin Chang <r09922117@csie.ntu.edu.tw>
2022-10-24 18:16:39 +0800

    add more cscope settings

--- neovim/.config/nvim/lua/rhythm/autocmds.lua
+++ neovim/.config/nvim/lua/rhythm/autocmds.lua
@@ -8,9 +8,17 @@
 -- ]]
 
 vim.cmd [[
+    " use both cscope and ctag for 'ctrl-]', ':ta', and 'vim -t'
+    set cscopetag
+    " check cscope definition of a symbol before checking ctags: set to 1
+    " if you want the reverse search order.
+    set csto=0
+    " add any cscope database in current directory
     if filereadable("cscope.out")
         cs add cscope.out
     endif
+    " show msg when any other cscope db added
+    set cscopeverbose
 ]]
 
 -- close quickfix when CR is hit
