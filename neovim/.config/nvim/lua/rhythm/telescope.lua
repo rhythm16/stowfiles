e998579522f7eaf8de5d5d6b795a62dfcbc90c00
Wei-Lin Chang <r09922117@csie.ntu.edu.tw>
2023-08-03 14:25:32 +0800

    use truncate instead of smart for telescope path display

--- neovim/.config/nvim/lua/rhythm/telescope.lua
+++ neovim/.config/nvim/lua/rhythm/telescope.lua
@@ -10,7 +10,7 @@ telescope.setup {
     defaults = {
 --        prompt_prefix = "",
 --        selection_caret = "",
-        path_display = { "smart" },
+        path_display = { "truncate" },
 --                shorten = { len = 5 },
 --                "absolute",
         mappings = {
