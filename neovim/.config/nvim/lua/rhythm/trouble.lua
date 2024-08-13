9d35ab3b5e620141a53bce3b2a9dd4ad48fb3c5a
Wei-Lin Chang <r09922117@csie.ntu.edu.tw>
2024-08-13 14:36:53 +0800

    make trouble show more symbols

--- neovim/.config/nvim/lua/rhythm/trouble.lua
+++ neovim/.config/nvim/lua/rhythm/trouble.lua
@@ -23,7 +23,7 @@ trouble.setup {
   restore = true, -- restores the last location in the list when opening
   follow = true, -- Follow the current item
   indent_guides = true, -- show indent guides
-  max_items = 200, -- limit number of items that can be displayed per section
+  max_items = 1000, -- limit number of items that can be displayed per section
   multiline = true, -- render multi-line messages
   pinned = false, -- When pinned, the opened trouble window will be bound to the current buffer
   warn_no_results = true, -- show a warning when there are no results
