be73bfabd06e715396a5257e61c07dd40e2c756f
Wei-Lin Chang <r09922117@csie.ntu.edu.tw>
2024-08-25 12:04:19 +0800

    make it easy to start neomutt for different accounts

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,8 @@
 set -g fish_greeting "Welcome to fish!"
 set -x GPG_TTY $(tty)
 set -x EDITOR nvim
+
+# personal
+alias pmail="neomutt -F ~/.config/neomutt/neomuttrc_personal"
+# work
+alias wmail="neomutt -F ~/.config/neomutt/neomuttrc_canonical"
