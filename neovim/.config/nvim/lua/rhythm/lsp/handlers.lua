b8d15d0b3c3ed94ee61a90b1fadc37db184a1b17
Wei-Lin Chang <r09922117@csie.ntu.edu.tw>
2025-05-31 19:14:48 +0800

    update diagnostics sign config api

--- neovim/.config/nvim/lua/rhythm/lsp/handlers.lua
+++ neovim/.config/nvim/lua/rhythm/lsp/handlers.lua
@@ -1,16 +1,6 @@
 local M = {}
 
 M.setup = function()
-    local signs = {
-      { name = "DiagnosticSignError", text = "" },
-      { name = "DiagnosticSignWarn", text = "" },
-      { name = "DiagnosticSignHint", text = "" },
-      { name = "DiagnosticSignInfo", text = "" },
-    }
-
-    for _, sign in ipairs(signs) do
-      vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-    end
 
     local config = {
         virtual_text = false,
@@ -25,8 +15,20 @@ M.setup = function()
             header = "",
             prefix = "",
         },
+
         signs = {
-            active = signs,
+            text = {
+                [vim.diagnostic.severity.ERROR] = '',
+                [vim.diagnostic.severity.WARN] = '',
+                [vim.diagnostic.severity.HINT] = '',
+                [vim.diagnostic.severity.INFO] = '',
+            },
+            texthl = {
+                [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+                [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+                [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+                [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+            },
         },
     }
 
